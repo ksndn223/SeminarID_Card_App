80bc72517d8275a8283b116acfc196b1fd279157
ksndn223 <11366993+ksndn223@users.noreply.github.com>
2023-12-25 18:49:01 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import MyCards from './components/MyCards';
+import SeminarCard from './components/SeminarCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h3> Welcome to Technology Group Seminar ID Card App!</h3>
+        
+        <MyCards/>
+        <SeminarCard/>
     </div>
+   
   );
 }
 
